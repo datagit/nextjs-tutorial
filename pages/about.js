8066e6a6767e760d23ab7474529482049acc8853
Dat Dao <datagit@yahoo.com>
2020-09-17 05:10:39 +0700

    ant design button examples

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,24 @@
+import React from "react";
 import Layout from '../components/Layout';
+import {Button}  from 'antd';
 
-const About = () => (
-    <Layout>
-        <div>
-            <h1>About BitsPrice</h1>
-            <p>Application to view Bitcoin prices</p>
-        </div>
-    </Layout>
-);
+const About = () => {
+    function handleClick() {
+        console.log('handleClick');
+    }
+    return (
+        <Layout>
+            <div>
+                <h1>About BitsPrice</h1>
+                <Button type="primary" onClick={handleClick}>Primary Button</Button>
+                <Button>Default Button</Button>
+                <Button type="dashed">Dashed Button</Button>
+                <br />
+                <Button type="text">Text Button</Button>
+                <Button type="link">Link Button</Button>
+            </div>
+        </Layout>
+    );
+}
 
 export default About;
